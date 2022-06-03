5f543e21e665dc6b55e915f33817b04b88004dcb
Johan Thoren <johan@thoren.xyz>
2022-06-03 16:26:01 +0200

    Version 0.1.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.itrsgroup/op5-license-inspector "0.1.5"
+(defproject com.itrsgroup/op5-license-inspector "0.1.6-SNAPSHOT"
   :description "Quickly extract information from OP5 lic files"
   :url "https://github.com/ITRS-Group/op5-license-inspector"
   :license {:name "MIT"
